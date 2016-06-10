03cff5d3a88bfc61dde60b72cd792eb5a0563ac6
Vinodh Thiagarajan <vinodhthiagarajan@Vinodhs-MacBook-Pro.local>
2016-06-09 20:43:16 -0500

    Formatted PYTHON file as per PEP 8 standards

--- moviesDb.py
+++ moviesDb.py
@@ -1,4 +1,4 @@
-#Download the required libraries
+# Download the required libraries
 import media
 import webbrowser
 import fresh_tomatoes
@@ -10,41 +10,47 @@ import urllib
    The JSON message is parsed we create Movie
    objects out of it.
    The mehtod returns a list of movie objects'''
+
+
 def get_top_rated_movies():
-    json_data =urllib.urlopen("https://api.themoviedb.org/3/movie/top_rated?api_key=14f6f23935d5f6396544342f801e171d")
-    movies_json_data = json.loads(json_data.read())  #JSON to Python Dictionary
+    BASE_URL = "https://api.themoviedb.org/3/movie/"
+    API_KEY = "api_key=14f6f23935d5f6396544342f801e171d"
+    IMAGE_URL = "http://image.tmdb.org/t/p/w500"
+    YOUTUBE_URL = "https://youtu.be/"
+    json_data = urllib.urlopen(BASE_URL + "top_rated?" + API_KEY)
+    movies_json_data = json.loads(json_data.read())  # JSON to Python Dict
     movies_list = []
-    iMovieCount = 0; #iniate a counter to count the movie objects being created
+    # iniate a counter to count the movie objects being created
+    iMovieCount = 0
     for movieObj in movies_json_data['results']:
-        title =  str(movieObj['title'])
-        poster =  str(movieObj['poster_path'])
-        overview =  str(movieObj['overview'].encode('utf-8')) #encoded version of the string
+        title = str(movieObj['title'])
+        poster = str(movieObj['poster_path'])
+        # encoded version of the string
+        overview = str(movieObj['overview'].encode('utf-8'))
         movie_id = str(movieObj['id'])
         trailer = ''
-
- 
-        videos_data = urllib.urlopen("https://api.themoviedb.org/3/movie/"+movie_id+"/videos?api_key=14f6f23935d5f6396544342f801e171d")
+        videos_data = urllib.urlopen(BASE_URL + movie_id+"/videos?" + API_KEY)
         video_json_data = json.loads(videos_data.read())
-
-        #Inner for loop recieves a list of movie trailers and we just need one of them and only from Youtube
-        #Break when link to one video is retreived
+        '''Inner for loop recieves a list of movie trailers and we just
+        need one of them and only from Youtube break when link to one video
+        is retreived'''
         for videoObj in video_json_data['results']:
-            if videoObj['site'] == 'YouTube' :
+            if videoObj['site'] == 'YouTube':
                 trailer = str(videoObj['key'])
                 break
 
-        #Create a Movie object and push it to a List
-        movies_list.append(media.Movie(title,overview,
-                       "http://image.tmdb.org/t/p/w500"+poster+"?api_key=14f6f23935d5f6396544342f801e171d" ,
-                       "https://youtu.be/"+trailer))
-
+        # Create a Movie object and push it to a List
+        movies_list.append(media.Movie(
+            title,
+            overview,
+            IMAGE_URL + poster + "?" + API_KEY,
+            YOUTUBE_URL + trailer)
+            )
+        '''Since we are only displaying only Top 15 movies break the loop once we
+        have created 15 movie objects'''
         iMovieCount += 1
-        #Since we are only displaying only Top 15 movies break the loop once we have created 15 movie objects
         if iMovieCount == 15:
             break
-           
     return movies_list
-
-
 
 fresh_tomatoes.open_movies_page(get_top_rated_movies())
